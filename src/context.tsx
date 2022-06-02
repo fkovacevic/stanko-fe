50db77acdf664b076cd642ba9858fee6b6b0d0a2
fkovacevic <fkovacevic00@gmail.com>
2022-06-02 14:54:30 +0200

    context bug fix

--- src/context.tsx
+++ src/context.tsx
@@ -4,22 +4,29 @@ import { firebaseAuth } from './firebase'
 
 interface AuthorizationContextType {
 	user: firebase.User | null,
+	isLoading: boolean,
 }
 
 const AuthorizationContext = createContext<AuthorizationContextType>({
 	user: null,
+	isLoading: false,
 });
 
 export const AuthorizationContextProvider = (props: any) => {
 	const [user, setUser] = useState<firebase.User | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
 	useEffect(() => {
+		setIsLoading(true);
 		firebaseAuth.onAuthStateChanged((user) => {
 			setUser(user);
+			setIsLoading(false);
 		});
 	}, [])
 
 	const authContextValue: AuthorizationContextType = {
 		user,
+		isLoading,
 	};
 
 	return <AuthorizationContext.Provider value={authContextValue}>
