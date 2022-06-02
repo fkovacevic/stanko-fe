aa2c0f1e4d996de0ad50e8babc83a77446684e1f
fkovacevic <fkovacevic00@gmail.com>
2022-06-03 00:01:00 +0200

    loading and proper routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 
 import React, { useEffect, useContext, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import AuthorizationContext from './context';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 import RequireAuth from 'common/hocs/RequireAuth';
 import './app.scss';
 
-import { collection, getFirestore } from '@firebase/firestore'
+import { collection, getFirestore, Query } from '@firebase/firestore'
 
 import Apartments from './pages/Apartments/index';
 import Notifications from './pages/Notifications/index';
@@ -23,6 +25,7 @@ import NavigationBar from 'common/components/NavigationBar';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import Login from 'pages/Login';
 import { firebaseAuth } from './firebase';
+import Register from 'pages/Register';
 
 
 require('firebase/auth');
@@ -71,15 +74,19 @@ function App() {
 	// }
 	// const apartmentsRef = firestore.collection('apartments');
 	// const query = apartmentsRef.orderBy('title').limit(5).withConverter(apartmentConverter);
-	// const [apartments, isLoading, error] = useCollection(collection(firestore, 'apartments'));
+	// const [apartments, isLoading, error] = useCollection(query);
 	// console.log(apartments?.docs.forEach((value) => console.log(value.data())));
-	const { user } = useContext(AuthorizationContext);
+	const { user, isLoading } = useContext(AuthorizationContext);
 
 	return (
-		<BrowserRouter>
-			{user && <NavigationBar />}
+		isLoading
+		? <div className='app-loading'>
+			<CircularProgress />
+		</div>
+		: <BrowserRouter>
+			<NavigationBar />
 			<Routes>
-				<Route path={`/`} element={<Homepage />}/>
+				<Route path='/' element={<Homepage />}/>
 				<Route path='/stanovi' element={
 					<RequireAuth>
 						<Apartments />
@@ -90,11 +97,12 @@ function App() {
 						<Notifications />
 					</RequireAuth>
 				} />
-				<Route path={`/oglasi`} element={
+				<Route path='/oglasi' element={
 					<RequireAuth>
 						<MyApartments />
 					</RequireAuth>
 				} />
+				<Route path='/registracija' element={<Register />} />
 				<Route path='*' element={<Login />} />
 			</Routes>
 		</BrowserRouter>
