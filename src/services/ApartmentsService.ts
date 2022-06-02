cadfcc68ab465bfb8ee0ca7ae9926625cc236df5
fkovacevic <fkovacevic00@gmail.com>
2022-06-03 00:07:43 +0200

    user and apartment services

--- src/services/ApartmentsService.ts
+++ src/services/ApartmentsService.ts
@@ -1,12 +1,26 @@
-import axios from "axios";
-import { firebaseAuth, firestore } from '../firebase';
-import firebase from 'firebase/compat/app';
-
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import ApartmentVM from 'models/ApartmentVM';
+import { enableIndexedDbPersistence, getDocs, getDocsFromCache } from "firebase/firestore";
 
-export const getApartments = async() => {
+
+import { firestore } from '../firebase';
+import ApartmentVM from 'models/ApartmentVM';
+import axios, { AxiosRequestConfig } from 'axios';
+import { STANKO_API_PATH } from 'constants/ApiPath';
+
+export const getApartmentsForUser = async (userId: string) => {
 	const apartmentsRef = firestore.collection('apartments');
-	return apartmentsRef.orderBy('title').limit(5).withConverter(ApartmentVM.apartmentConverter).get();
+	return apartmentsRef.orderBy('createdAt').withConverter(ApartmentVM.apartmentConverter).where('createdById', '==', userId).get();
 }
+
+export const deleteApartment = async (apartment: ApartmentVM) => {
+	const { id } = apartment;
+	const config: AxiosRequestConfig = {
+		data: { apartment },
+	};
+
+	const url = `${STANKO_API_PATH}/apartments/${id}`;
+
+	return await axios.delete(url, config);
+}
+
