da63003d19311160f94204b9b93774482d5c9ef8
fkovacevic <fkovacevic00@gmail.com>
2022-06-03 00:06:00 +0200

    navigation bar full functionality

--- src/common/components/NavigationBar/index.tsx
+++ src/common/components/NavigationBar/index.tsx
@@ -1,105 +1,121 @@
-import { useEffect, useState, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState, useRef, useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Grid, Button, ButtonGroup, Popover, Paper, MenuItem, MenuList } from '@material-ui/core';
-import {  GiHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { FiLogOut } from 'react-icons/fi'
 
+import useDeviceWidth from 'common/custom-hooks/useDeviceWidth';
 import { firebaseAuth } from '../../../firebase'
 import './_navigation-bar.scss';
+import { unsubscribeUserForPushSubscription } from 'services/UserService';
+import { unsubscribeUser } from 'serviceWorkerSubscription';
+import AuthorizationContext from 'context';
 
-const MOBILE_WIDTH_THRESHOLD = 1000;// px
+
 
 const NavigationBar = () => {
-    let navigate = useNavigate();
+	let navigate = useNavigate();
+	const location = useLocation();
+	const { user } = useContext(AuthorizationContext);
 
-    const [mobileMode, setMobileMode] = useState<boolean>(window.innerWidth < MOBILE_WIDTH_THRESHOLD);
-    const [anchorEl, setAnchorEl] = useState(null);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const deviceType = useDeviceWidth();
 
-    const menuButtonRef =  useRef(null);
+	const menuButtonRef = useRef(null);
 
-    function handleMenuClick (event: any) {
-        setAnchorEl(menuButtonRef.current);
-    }
-    function handleMenuClose (event: any) {
-        setAnchorEl(null);
-    }
-    function resizeHandler() {
-        if (window.innerWidth < MOBILE_WIDTH_THRESHOLD) {
-            setMobileMode(true);
-        } else {
-            setMobileMode(false);
-        }
-    }
-    function handleLogOut() {
-        firebaseAuth.signOut().then(() => navigate('/'));
-    }
+	if (location.pathname === '/prijava' || location.pathname === '/registracija') {
+		return null;
+	}
 
-    useEffect(() => {
-        window.addEventListener('resize', resizeHandler);
-    }, []);
+	function handleMenuClick(event: any) {
+		setAnchorEl(menuButtonRef.current);
+	}
+	function handleMenuClose(event: any) {
+		setAnchorEl(null);
+	}
 
-    return (
-        <Grid container className='navigation-bar'>
-            <Grid item xs={8} className='navigation-bar__logo-wrapper'>
-                <span className="navigation-bar__stanko">
-                    <img src="stanko.svg" alt="stanko" />
-                </span>
-            </Grid>
-            <Grid item xs={4}>
-                {mobileMode ?
-                    <div className="navigation-bar__menu">
-                        <Button
-                            ref={menuButtonRef}
-                            onClick={handleMenuClick}
-                        >
-                            <GiHamburgerMenu className='navigation-bar__menu__icon'/>
-                        </Button>
-                        <Popover
-                            open={!!anchorEl}
-                            anchorEl={anchorEl}
-                            onClose={handleMenuClose}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "center"
-                            }}
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "center"
-                            }}
-                        >
-                             <Paper >
-                                <MenuList className="navigation-bar__menu__popover">
-                                    <MenuItem component={Link} to='stanovi'>stanovi</MenuItem>
-                                    <MenuItem component={Link} to='/obavijesti'>obavijesti</MenuItem>
-                                    <MenuItem component={Link} to='/oglasi'>oglasi</MenuItem>
-                                    <MenuItem onClick={handleLogOut}>
-                                        <FiLogOut />
-                                    </MenuItem>
-                                </MenuList>
-                            </Paper>
-                        </Popover>
-                    </div>
-                    :
-                    <ButtonGroup variant="outlined" aria-label="text button group">
-                        <Link to='/stanovi' style={{ 'textDecoration': 'none' }}>
-                            <Button className='navigation-bar__button navigation-bar__hover-animation' >Stanovi</Button>
-                        </Link>
-                        <Link to='/obavijesti' style={{ 'textDecoration': 'none' }}>
-                            <Button className='navigation-bar__button navigation-bar__hover-animation'>Obavijesti</Button>
-                        </Link>
-                        <Link to='/oglasi' style={{ 'textDecoration': 'none' }}>
-                            <Button className='navigation-bar__button navigation-bar__hover-animation'>Oglasi</Button>
-                        </Link>
-                        <div style={{ 'textDecoration': 'none', 'display': 'flex' }}>
-                            <Button className='navigation-bar__button navigation-bar__hover-animation' onClick={handleLogOut}>
-                                <FiLogOut className='navigation-bar__log-out-button'/>
-                            </Button>
-                        </div>
-                    </ButtonGroup>
-                }
-            </Grid>
-        </Grid>
-    );
+
+	async function handleLogOut() {
+		const pushSubscription = await unsubscribeUser();
+		const userId = user?.uid;
+		if (pushSubscription && userId) {
+		    await unsubscribeUserForPushSubscription(userId, pushSubscription);
+		}
+		firebaseAuth.signOut().then(() => navigate('/prijava'));
+	}
+
+	const mobileOrTabletMode = deviceType === 'MOBILE_WIDTH' || deviceType === 'TABLET_WIDTH';
+	const isOnHomepage = location.pathname === '/';
+	const buttonClassName = `navigation-bar__button ${isOnHomepage ? 'navigation-bar--button-homepage' : ''} navigation-bar__hover-animation`;
+
+	return (
+		<Grid container className={`navigation-bar ${isOnHomepage ? 'navigation-bar--homepage' : ''}`}>
+			<Grid item xs={8} className='navigation-bar__logo-wrapper'>
+				<Link to='/'>
+					<span className="navigation-bar__stanko">
+						<img src="stanko.svg" alt="stanko" />
+					</span>
+				</Link>
+			</Grid>
+			<Grid item xs={4}>
+				{mobileOrTabletMode ?
+					<div className="navigation-bar__menu">
+						<Button
+							ref={menuButtonRef}
+							onClick={handleMenuClick}
+						>
+							<GiHamburgerMenu className='navigation-bar__menu__icon' />
+						</Button>
+						<Popover
+							open={!!anchorEl}
+							anchorEl={anchorEl}
+							onClose={handleMenuClose}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "center"
+							}}
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "center"
+							}}
+						>
+							<Paper >
+								<MenuList className="navigation-bar__menu__popover">
+									<MenuItem component={Link} to='stanovi'>stanovi</MenuItem>
+									<MenuItem component={Link} to='/obavijesti'>obavijesti</MenuItem>
+									<MenuItem component={Link} to='/oglasi'>oglasi</MenuItem>
+									{user &&
+										<MenuItem onClick={handleLogOut}>
+											<FiLogOut />
+										</MenuItem>
+									}
+								</MenuList>
+							</Paper>
+						</Popover>
+					</div>
+					:
+					<ButtonGroup variant="outlined" aria-label="text button group">
+						<Link to='/stanovi' style={{ 'textDecoration': 'none' }}>
+							<Button className={buttonClassName}>Stanovi</Button>
+						</Link>
+						<Link to='/obavijesti' style={{ 'textDecoration': 'none' }}>
+							<Button className={buttonClassName}>Obavijesti</Button>
+						</Link>
+						<Link to='/oglasi' style={{ 'textDecoration': 'none' }}>
+							<Button className={buttonClassName}>Oglasi</Button>
+						</Link>
+						<div style={{ 'textDecoration': 'none', 'display': 'flex' }}>
+							{user &&
+								<Button className={buttonClassName} onClick={handleLogOut}>
+									<FiLogOut className='navigation-bar__log-out-button' />
+								</Button>
+							}
+						</div>
+					</ButtonGroup>
+				}
+			</Grid>
+		</Grid>
+	);
 };
 
 export default NavigationBar;
