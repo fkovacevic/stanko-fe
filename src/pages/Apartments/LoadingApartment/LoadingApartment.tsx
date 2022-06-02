5d9c17aec3b82949f5a92145029b6f0d696a5755
fkovacevic <fkovacevic00@gmail.com>
2022-06-03 00:14:29 +0200

    loading apartment component

--- src/pages/Apartments/LoadingApartment/LoadingApartment.tsx
+++ src/pages/Apartments/LoadingApartment/LoadingApartment.tsx
@@ -3,10 +3,16 @@ import { Grid } from "@material-ui/core";
 import { Skeleton } from '@mui/material';
 
 import './_loading-apartment.scss'
+import useDeviceWidth from 'common/custom-hooks/useDeviceWidth';
 
 const LoadingApartment = () => {
+	const deviceType = useDeviceWidth();
+
+	const isPC = deviceType === 'PC_WIDTH';
+	const gridWidth = isPC ? 6 : 12;
+
 	return (
-		<Grid xs={6} item className='loading-apartment-container'>
+		<Grid xs={gridWidth} item className='loading-apartment-container'>
 			<Skeleton variant="rectangular" height={'30vh'}/>
 			<Skeleton variant="text" height={'5vh'}/>
 			<Skeleton variant="text" height={'5vh'} />
