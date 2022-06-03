ca2ebfd1c9630863c68d89c1635170f726bcf914
fkovacevic <fkovacevic00@gmail.com>
2022-06-03 21:16:21 +0200

    fixed user subscription loading

--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -2,9 +2,10 @@ import { useState, useContext, useEffect } from 'react';
 import { FormControl, Grid, InputLabel, MenuItem, Select, Slider, Button, Switch, FormControlLabel } from '@material-ui/core';
 import { Snackbar, Alert } from '@mui/material';
 import { IoMdSave } from 'react-icons/io';
+import { CircularProgress } from '@material-ui/core';
 
 import { setUserSubscription, getUserSubscription } from 'services/UserService'
-import { PartOfTown, partsOfTown } from 'models/ApartmentVM'
+import { partsOfTown } from 'models/ApartmentVM'
 import { UserSubscription } from 'models/UserVM';
 import './_notifications.scss';
 import AuthorizationContext from 'context';
@@ -14,7 +15,7 @@ function renderPriceLabel(value: number) {
 	return <div>{`${value}€`}</div>;
 }
 
-function renderAreaLabel (value: number) {
+function renderAreaLabel(value: number) {
 	return <div>{`${value}`}</div>
 }
 function renderPriceAiaLabel() {
@@ -36,19 +37,21 @@ const Notifications = () => {
 	const [turnOffNotifications, setTurnOffNotifications] = useState(false);
 	const [openSuccessAlert, setopenSuccessAlert] = useState(false);
 	const [openNotificationChangeAlert, setOpenNotificationChangeAlert] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const deviceType = useDeviceWidth();
 
 	useEffect(() => {
-		async function fetchUserSubscription () {
+		async function fetchUserSubscription() {
+			setIsLoading(true);
 			const userSubscriptionSnapshot = await getUserSubscription(user?.uid);
 			const userSubscription = userSubscriptionSnapshot.data()?.userSubscription;
 
-			console.log(userSubscription);
 			let usKeys: string[] = [];
 			if (userSubscription) {
 				usKeys = Object.keys(userSubscription);
 			}
 			if (usKeys.length > 0) {
+				console.log('postoji user subscription')
 				setPartOfTown(userSubscription?.partOfTown!);
 				setPriceMin(userSubscription?.priceMin!);
 				setpriceMax(userSubscription?.priceMax!);
@@ -58,6 +61,7 @@ const Notifications = () => {
 			} else {
 				setTurnOffNotifications(true);
 			}
+			setIsLoading(false);
 		}
 		fetchUserSubscription();
 	}, [user?.uid])
@@ -77,10 +81,12 @@ const Notifications = () => {
 	};
 
 	async function handleSwitchChange(value: any) {
-		setTurnOffNotifications(!value.currentTarget.checked);
 		if (value.currentTarget.checked) {
 			await setUserSubscription(user?.uid, {});
 			setOpenNotificationChangeAlert(true);
+			setTurnOffNotifications(true);
+		} else {
+			setTurnOffNotifications(false);
 		}
 	}
 
@@ -99,7 +105,7 @@ const Notifications = () => {
 		}, 10000);
 	}
 
-	function handleCloseSuccessAlert () {
+	function handleCloseSuccessAlert() {
 		setopenSuccessAlert(false);
 	}
 
@@ -114,92 +120,102 @@ const Notifications = () => {
 	return (
 		<div className='notifications-container'>
 			<div className='notifications'>
-				<div className='notifications__title'>
-					Postavi obavijesti
-				</div>
-				<Grid container className='notifications__part-of-town'>
-					<Grid item xs={gridWidth}>
-						<Grid container>
-							<Grid item xs={12}>
-								<FormControl fullWidth>
-									<InputLabel id="part-of-town-dropdown">Kvart</InputLabel>
-									<Select
-										labelId="part-of-town-dropdown"
-										id="part-of-town-dropdown-id"
-										label="Kvart"
-										onChange={handlePartOfTownChange}
-										defaultValue=''
-										className='notifications__part-of-town__dropdown'
-										disabled={isFormDisabled}
-									>
-										{partsOfTown.map((pot) => <MenuItem value={pot} key={pot}>{pot}</MenuItem>)}
-									</Select>
-								</FormControl>
-							</Grid>
-							<Grid item xs={12}>
-								<div className='notifications__slider-label'>
-									{`Cijena ${priceMin}(€) - ${priceMax}(€)`}
-								</div>
-								<Slider
-									getAriaLabel={renderPriceAiaLabel}
-									valueLabelFormat={renderPriceLabel}
-									value={[priceMin, priceMax]}
-									step={10}
-									min={100}
-									max={1500}
-									onChange={handlePriceRange}
-									valueLabelDisplay="auto"
-									getAriaValueText={renderPriceAiaLabel}
-									className='notifications__slider'
-									disabled={isFormDisabled}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<div className='notifications__slider-label'>
-									{`Površina ${areaMin}m2 - ${areaMax}m2`}
-								</div>
-								<Slider
-									getAriaLabel={renderAreaAriaLabel}
-									valueLabelFormat={renderAreaLabel}
-									value={[areaMin, areaMax]}
-									min={10}
-									max={200}
-									step={5}
-									onChange={handleAreaRange}
-									valueLabelDisplay="auto"
-									getAriaValueText={renderAreaAriaLabel}
-									className='notifications__slider'
-									disabled={isFormDisabled}
-								/>
-							</Grid>
-							<Grid item xs={12} className='notifications__save-button'>
-								<Button
-									endIcon={<IoMdSave />}
-									onClick={saveUserSubscription}
-									disabled={isFormDisabled}
-								>
-									Spremi
-								</Button>
-							</Grid>
-							<Grid container>
-								<Grid item xs={12} className='notifications__turn-off'>
-									<span className='notifications__switch-label'>
-										Prestani primati obavijesti?
-									</span>
-									<Switch color='secondary' value={turnOffNotifications} onChange={handleSwitchChange}/>
+				{isLoading
+					? <div className='notifications__loading'>
+						<CircularProgress />
+					</div>
+					: <>
+						<div className='notifications__title'>
+							Postavi obavijesti
+						</div>
+						<Grid container className='notifications__part-of-town'>
+							<Grid item xs={gridWidth}>
+								<Grid container>
+									<Grid item xs={12}>
+										<FormControl fullWidth>
+											<InputLabel id="part-of-town-dropdown">Kvart</InputLabel>
+											<Select
+												labelId="part-of-town-dropdown"
+												id="part-of-town-dropdown-id"
+												label="Kvart"
+												onChange={handlePartOfTownChange}
+												defaultValue={partOfTown ?? ''}
+												className='notifications__part-of-town__dropdown'
+												disabled={isFormDisabled}
+											>
+												{partsOfTown.map((pot) => <MenuItem value={pot} key={pot}>{pot}</MenuItem>)}
+											</Select>
+										</FormControl>
+									</Grid>
+									<Grid item xs={12}>
+										<div className='notifications__slider-label'>
+											{`Cijena ${priceMin}(€) - ${priceMax}(€)`}
+										</div>
+										<Slider
+											getAriaLabel={renderPriceAiaLabel}
+											valueLabelFormat={renderPriceLabel}
+											value={[priceMin, priceMax]}
+											step={10}
+											min={100}
+											max={1500}
+											onChange={handlePriceRange}
+											valueLabelDisplay="auto"
+											getAriaValueText={renderPriceAiaLabel}
+											className='notifications__slider'
+											disabled={isFormDisabled}
+										/>
+									</Grid>
+									<Grid item xs={12}>
+										<div className='notifications__slider-label'>
+											{`Površina ${areaMin}m2 - ${areaMax}m2`}
+										</div>
+										<Slider
+											getAriaLabel={renderAreaAriaLabel}
+											valueLabelFormat={renderAreaLabel}
+											value={[areaMin, areaMax]}
+											min={10}
+											max={200}
+											step={5}
+											onChange={handleAreaRange}
+											valueLabelDisplay="auto"
+											getAriaValueText={renderAreaAriaLabel}
+											className='notifications__slider'
+											disabled={isFormDisabled}
+										/>
+									</Grid>
+									<Grid item xs={12} className='notifications__save-button'>
+										<Button
+											endIcon={<IoMdSave />}
+											onClick={saveUserSubscription}
+											disabled={isFormDisabled}
+										>
+											Spremi
+										</Button>
+									</Grid>
+									<Grid container>
+										<Grid item xs={12} className='notifications__turn-off'>
+											<span className='notifications__switch-label'>
+												Prestani primati obavijesti?
+											</span>
+											<Switch
+												checked={turnOffNotifications}
+												onChange={handleSwitchChange}
+												inputProps={{ 'aria-label': 'controlled' }}
+											/>
+										</Grid>
+									</Grid>
 								</Grid>
 							</Grid>
 						</Grid>
-					</Grid>
-				</Grid>
+					</>}
 			</div>
 			<Snackbar
 				open={openSuccessAlert}
-				anchorOrigin={{vertical: 'top', horizontal: 'right' }}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
 				onClose={handleCloseSuccessAlert}
 			>
 				<Alert severity='success'>
-					<div style={{'color': 'green'}}>
+					<div style={{ 'color': 'green' }}>
 						<b>
 							Uspješno ste postavili postavke za notifikacije:
 						</b>
@@ -217,11 +233,11 @@ const Notifications = () => {
 			</Snackbar>
 			<Snackbar
 				open={openNotificationChangeAlert}
-				anchorOrigin={{vertical: 'top', horizontal: 'right' }}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
 				onClose={handleCloseNotificationChangeAlert}
 			>
 				<Alert severity='success'>
-					<div style={{'color': 'green'}}>
+					<div style={{ 'color': 'green' }}>
 						<b>
 							Uspješno ste ugasili obavijesti.
 						</b>
